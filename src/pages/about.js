b70ea1f4657d6e2ab3dce4a296e0087eb38a2d34
kxdang <ikien.dang@gmail.com>
2020-04-17 00:30:24 -0400

    editing about

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,15 +11,13 @@ const AboutPage = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="About" />
       <div style={{ marginTop: `30px`, fontFamily: `Montserrat` }}>
-        <h2>Hey</h2>
-        My name is Duong Thuy Nguyen. I also go by Maria.
+        <h2 style={{ textAlign: "center" }}>Hey, I'm Duong Thuy Nguyen.👋</h2>
+        <p style={{ textAlign: "center" }}>I also go by Maria.</p>
 
-I am an illustrator and I'm here to purge my visual creation onto anyone who doesn't mind the mess :) I am currently residing in a city just outside of Toronto with two cats I'm trying to convince to love me more. I also enjoy gardening, the colour hot pink, horror manga, pop music and listening to interviews with rappers.
+        <p>I am an illustrator and I'm here to purge my visual creation onto anyone who doesn't mind the mess :) I am currently residing in a city just outside of Toronto with two cats I'm trying to convince to love me more. I also enjoy gardening, the colour hot pink, horror manga, pop music and listening to interviews with rappers.</p>
 
-My illustrations for Reveal News' Instagram post for an investigative article about exploiting human labour were among the submitted materials to earn a spot as a Pulitzer finalist in 2017. I am also a staff illustrator for the Global Press Accuracy Network.
-        <a href="https://22boxes.com" target="blank">
-          22Boxes.com
-        </a>
+        <p>My illustrations for Reveal News' Instagram post for an investigative article about exploiting human labour were among the submitted materials to earn a spot as a Pulitzer finalist in 2017. I am also a staff illustrator for the Global Press Accuracy Network.</p>
+
       </div>
     </Layout>
   )
