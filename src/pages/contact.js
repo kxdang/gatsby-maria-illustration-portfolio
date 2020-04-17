6b915aee7cc3e097f9222415c94dd08b9e6fc9d9
kxdang <ikien.dang@gmail.com>
2020-04-17 15:00:25 -0400

    finished contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,73 @@
 import React from "react"
 import { graphql } from "gatsby"
-
+import Obfuscate from "react-obfuscate";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FaEnvelopeOpen, FaTwitter, FaPatreon, FaInstagram, FaImage } from "react-icons/fa";
 
 const AboutPage = ({ data, location }) => {
-    const siteTitle = data.site.siteMetadata.title
+  const siteTitle = data.site.siteMetadata.title
 
-    return (
-        <Layout location={location} title={siteTitle}>
-            <SEO title="Contact" />
-            <div style={{ marginTop: `30px`, fontFamily: `Montserrat` }}>
-                <h2>Contact</h2>
-        First Gatsby template by{" "}
-                <a href="https://22boxes.com" target="blank">
-                    22Boxes.com
-        </a>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Contact" />
+      <div style={{ marginTop: `30px`, fontFamily: `Montserrat` }}>
+        <h2 style={{ textAlign: `center` }}>Contact</h2>
+        <ul className="contact-list">
+          <li className="contact-item">
+            <FaTwitter />
+            <a
+              href="https://twitter.com/DTNart"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              DTNart
+          </a>
+          </li>
+
+          <li className="contact-item">
+            <FaEnvelopeOpen />
+            <Obfuscate email="hello@dtnart.com" />
+          </li>
+
+          <li className="contact-item">
+            <FaPatreon />
+            <a
+              href="https://www.patreon.com/dtnart"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              dtnart
+          </a>
+          </li>
+
+          <li className="contact-item">
+            <FaImage />
+            <a
+              href="https://www.inprnt.com/gallery/dtnart/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Prints
+          </a>
+          </li>
+
+          <li className="contact-item">
+            <FaInstagram />
+            <a
+              href="https://www.patreon.com/dtnart"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              dtnart
+          </a>
+          </li>
+        </ul>
+
+
+      </div>
+    </Layout>
+  )
 }
 
 export default AboutPage
